50eceae3f469ce39c722f79d3c1830928990141f
Austin Lindenberg <austindlindenber@ufl.edu>
2017-04-17 23:47:06 -0400

    Multiple sounds working at once

--- 3d-audio/src/SoundPanel/SoundPanel.js
+++ 3d-audio/src/SoundPanel/SoundPanel.js
@@ -6,82 +6,157 @@ import './SoundPanel.css';
 import E220 from '../E220/E220';
 import Carleton from '../Carleton/Carleton';
 
-window.load = windowLoad();
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
+window.onload = init;
+var context;
+var bufferLoader;
+var bufferArray = [];
 
-var audioBuffer = null;
-var context = new AudioContext();
-var sound = {};
-sound.source = null;
+var teacherSound = {};
+var constructionSound = {};
 
-sound.volume = context.createGain();
+var activeSounds = [];
 
-sound.panner = context.createPanner();
+function init() {
+	// Fix up prefixing
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	context = new AudioContext();
 
-// Instead of hooking up the volume to the main volume, hook it up to the panner.
-sound.volume.connect(sound.panner);
-// And hook up the panner to the main volume.
-sound.panner.connect(context.destination);
-
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
-    xhr.responseType = 'arraybuffer';
-
-  } else if (typeof XDomainRequest !== "undefined") {
-
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-    xhr.responseType = 'arraybuffer';
-
-  } else {
-
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
-
-  }
-  return xhr;
+  	bufferLoader = new BufferLoader(
+	    context,
+	    [
+	      'https://raw.githubusercontent.com/alindenberg/3dAudioSounds/master/sounds/ahem.wav',
+	      'https://raw.githubusercontent.com/alindenberg/3dAudioSounds/master/sounds/construction.wav',
+	    ],
+	    finishedLoading
+   	);
+	bufferLoader.load();
 }
 
-function windowLoad() {
-  var xhr = createCORSRequest("GET", "https://raw.githubusercontent.com/alindenberg/3dAudioSounds/master/sounds/ahem.wav");
-  if (!xhr) {
-    throw new Error('CORS not supported');
-  } else {
-      xhr.onload = function() {
-        context.decodeAudioData(xhr.response, function(buffer) {
-          audioBuffer = buffer;
-        }, error);
+function finishedLoading(bufferList) {
+
+	bufferArray = bufferList;
+
+	teacherSound.volume = context.createGain();
+	teacherSound.panner = context.createPanner();
+	teacherSound.volume.connect(teacherSound.panner);
+	teacherSound.panner.connect(context.destination);
+	
+	// var teacherSource = context.createBufferSource();
+	// teacherSource.buffer = bufferList[0];
+	// teacherSource.connect(teacherSound.volume);
+	// teacherSound.source = teacherSource;
+
+	constructionSound.volume = context.createGain();
+	constructionSound.panner = context.createPanner();
+	constructionSound.volume.connect(constructionSound.panner);
+	constructionSound.panner.connect(context.destination);
+	
+}
+
+function playSounds(teacherOn, tsX, tsY, tsZ, constructionOn) {
+
+	// teacherSound.panner.setPosition(x, y, z);
+	console.log(teacherOn);
+	console.log(constructionOn);
+	// THIS WILL BE SEAT LOCATION
+  	context.listener.setPosition(20, 15, 15);
+
+	if(teacherOn) {
+		// Create new source
+		var teacherSource = context.createBufferSource();
+		teacherSource.buffer = bufferArray[0];
+		teacherSource.connect(teacherSound.volume);
+
+		// Set position of sound
+		teacherSound.panner.setPosition(tsX,tsY, tsZ);
+
+		// Set source and play sound
+		teacherSound.source = teacherSource;
+		teacherSound.source.loop = true;
+		teacherSound.source.start(0);
+		activeSounds.push(teacherSound.source);
+	}
+	if(constructionOn) {
+		// Create new source
+  		var constructionSource = context.createBufferSource();
+		constructionSource.buffer = bufferArray[1];
+		constructionSource.connect(constructionSound.volume);
+
+		// Set position of sound
+		constructionSound.panner.setPosition(0,0,10);
+
+		// Set source and play sound
+		constructionSound.source = constructionSource;
+		constructionSound.source.loop = true;
+		constructionSound.source.start(0);
+  		activeSounds.push(constructionSound.source);
+	}
+}
+
+function stopSounds() {
+	for(var i = 0; i < activeSounds.length; i++)
+		activeSounds[i].stop(0);
+	activeSounds = [];
+}
+
+// Buffer class
+function BufferLoader(context, urlList, callback) {
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+  // Load buffer asynchronously
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  var loader = this;
+
+  request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        if (!buffer) {
+          alert('error decoding file data: ' + url);
+          return;
+        }
+        loader.bufferList[index] = buffer;
+        if (++loader.loadCount === loader.urlList.length)
+          loader.onload(loader.bufferList);
+      },
+      function(error) {
+        console.error('decodeAudioData error', error);
       }
-      xhr.send();
+    );
   }
+
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
+
+  request.send();
 }
 
-function error() {
-  console.log("Error decoding data");
+BufferLoader.prototype.load = function() {
+  for (var i = 0; i < this.urlList.length; ++i)
+  this.loadBuffer(this.urlList[i], i);
 }
 
-function playSound(x, y, z) {
-  sound.source = context.createBufferSource();
-  sound.source.buffer = audioBuffer;                    // tell the source which sound to play
-  sound.source.connect(sound.volume);       // connect the source to the context's destination (the speakers)
-  sound.panner.setPosition(0,0,0);
-  context.listener.setPosition(x, y, z);
-  sound.source.start(0);                           // play the source now
-  console.log("Playing sound");
-}
+// End Buffer class
+
 
 class SoundPanel extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			professorTalking:false,
+			professorLocation: 0,
+			constructionSound: false,
 			studentSittingNearby: false,
 			airConditioningOn: false,
 			nearbyChatter: false,
@@ -128,26 +203,6 @@ class SoundPanel extends Component {
 							ToggleButton/>
 						</li>
 						<li>
-							<h3>Air Conditioning</h3>
-							<ToggleButton 
-								value={ this.state.airConditioningOn || false } onToggle={
-									(value) => {
-								    	this.setState({airConditioningOn: !value,});
-								  	}
-							 	}
-							ToggleButton/>
-						</li>
-						<li>
-							<h3>Nearby Chatter</h3>
-							<ToggleButton 
-								value={ this.state.nearbyChatter || false } onToggle={
-									(value) => {
-								    	this.setState({nearbyChatter: !value,});
-								  	}
-							 	}
-							ToggleButton/>
-						</li>
-						<li>
 							<h3>Distant Chatter</h3>
 							<ToggleButton 
 								value={ this.state.distantChatter || false } onToggle={
@@ -158,12 +213,37 @@ class SoundPanel extends Component {
 							ToggleButton/>
 						</li>
 						<li>
+							<h3>Construction Outside</h3>
+							<ToggleButton 
+								value={ this.state.constructionSound || false } onToggle={
+									(value) => {
+								    	this.setState({constructionSound: !value,});
+								  	}
+							 	}
+							ToggleButton/>
+						</li>
+						<li>
+							<h3>Air Conditioning</h3>
+							<ToggleButton 
+								value={ this.state.airConditioningOn || false } onToggle={
+									(value) => {
+								    	this.setState({airConditioningOn: !value,});
+								  	}
+							 	}
+							ToggleButton/>
+						</li>
+						<li>
 							<h3>Volume</h3>
 							<Slider defaultValue='50' id='volumeSlider'></Slider>
 						</li>
 					</ul>
 					<div id="playSound">
-						<button id="playButton" onClick={()=>playSound(this.state.seatX,this.state.seatY,this.state.seatZ)}>Play sounds</button>
+						<button id="playButton" onClick={()=>playSounds(this.state.professorTalking, 
+																		this.state.seatX, this.state.seatY, this.state.seatZ, 
+																		this.state.constructionSound)}>Play sounds</button>
+					</div>
+					<div id="stopSound">
+						<button id="playButton" onClick={()=>stopSounds()}>Stop sounds</button>
 					</div>
 				</div>
 			</div>
